cd1425f2940f91719e2bcaba86c02190805abf4e
tuitstudent2025-debug <tuitstudent2025@gmail.com>
2026-04-25 21:48:00 +0500

    Add storage.js and fix import

---
+++ src/storage.js
@@ -0,0 +1,9 @@
+export const storage = {
+  async get(key) {
+    const value = localStorage.getItem(key);
+    return value ? { value } : null;
+  },
+  async set(key, value) {
+    localStorage.setItem(key, value);
+  },
+};
